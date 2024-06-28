5d5f08e8e1fe7dd05ce968b35446bc5055319eb7
TayyabNisar11 <tayyab.nisar.7719@gmail.com>
2024-06-28 23:39:23 +0500

    .

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,13 +2,17 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Sidebar from "./sidebar";
+import { useParams } from "next/navigation";
 
 const drawerWidth = 300;
 
 const Layout = ({ children }) => {
+  const params =useParams();
+
+  console.log(params)
   return (
     <Box sx={{ display: "flex" }}>
-      <Sidebar role="entrepreneur" />
+      <Sidebar role={params?.role?params.role:"entrepreneur"} />
       <Box
         component="main"
         sx={{
